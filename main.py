bfb9e6007ad78601cdea88b5cd4ba521385b447b
sopleb <sopleb@pm.me>
2026-02-24 12:24:13 -0500

    Add basic player and circle shape classes, integrate player rendering into main game loop
    - Introduced `CircleShape` base class in `circleshape.py` for reusable game object behavior (position, velocity, radius).
    - Added `Player` class in `player.py`, inheriting from `CircleShape`, with custom triangular representation and drawing logic.
    - Defined `PLAYER_RADIUS` and `LINE_WIDTH` constants in `constants.py`.
    - Updated `main.py` to initialize, update, and render the `Player` object within the game loop.

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import pygame
 
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     print(f"Screen width: {SCREEN_WIDTH}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
             pass
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
